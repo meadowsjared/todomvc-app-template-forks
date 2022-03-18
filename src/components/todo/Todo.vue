0bc956795012853906ce23e6fcd7e1581f1e0fa5
Kayla <kayla.bowser@yahoo.com>
2022-03-17 18:39:29 -0700

    created component, created interface

---
+++ src/components/todo/Todo.vue
@@ -0,0 +1,10 @@
+<template>
+	<li class="completed">
+          <div class="view">
+            <input class="toggle" type="checkbox" checked />
+            <button class="destroy"></button>
+            <label>Taste JavaScript</label>
+          </div>
+          <input class="edit" value="Create a TodoMVC template" />
+        </li>
+</template>
