838d5b45b4e314d149ab048768e67897a360e07d
Amarandus <secrest.cory@gmail.com>
2022-03-17 21:23:16 -0600

    Texts and Checks

--- src/Notes/Notes.ts
+++ src/Notes/Notes.ts
@@ -1,5 +1,25 @@
 /*
+
 {} = Object
-"" = Sting
+
+"" = Sting (Generally on property)
+
 [] = Array
+
+<component to import />
+
+{{}} = Display variable/text as text in on web page.
+
+//<script setup lang="ts">
+place for code
+</script>//
+
+Example for index/key of part (for loop):
+<Todo v-for="(todo,index) of todoArray" :key="index" :todo="todo"/>
+*/
+/* Commands-
+ctrl+p = search bar
+pnm dev = Run
+alt+shift+down arrow = copy to next line (just select end of line)
+ctrl+a > ctrl+shift+p > Wrap with abreviation > Type whatever to add tot op and bottom line
 */
