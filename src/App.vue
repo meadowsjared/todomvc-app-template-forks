7b9d82c41d100ded98ae4db12f5cead4b98b1ced
Kayla <kayla.bowser@yahoo.com>
2022-03-17 20:22:49 -0700

    Added For loops and double curly braces

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,7 @@
       <ul class="todo-list">
         <!-- These are here just to show the structure of the list items -->
         <!-- List items should get the class `editing` when editing and `completed` when marked as completed -->
-        <Todo />
-        <Todo />
+        <Todo v-for='(todo,index) of todoArray' :key='index' :todo='todo'/>
       </ul>
     </section>
     <!-- This footer should be hidden by default and shown when there are todos -->
@@ -35,20 +34,18 @@
       <button class="clear-completed">Clear completed</button>
     </footer>
   </section>
+	{{todoArray.length}}
 </template>
 
 <script setup lang='ts'>
 import type {Todo} from "./domain/Todo"
-const todos:Todo[] = [{
-    checked: true,
-    message: "things"
-},{
-    checked: false,
-    message: "stuff"
-}
+const todoArray:Todo[] = [
+	{checked: true,message: "Stay Positive"},
+	{checked: false,message: "Love Myself"},
+	{checked: true,message: "Be Grateful"},
 ]
 const numbers= [1,6,4,3,23,45,76]
 let message:string="hello world"
 
-console.log(message,1,5,numbers,todos);
+console.log(message,1,5,numbers,todoArray);
 </script>
