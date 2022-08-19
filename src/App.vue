4496d88e50b46bc701cce148920a0ac82a915280
DayCHU <herooftime711@gmail.com>
2022-08-18 17:11:39 -1000

    changed console.log to print sortState

--- src/App.vue
+++ src/App.vue
@@ -126,7 +126,15 @@ function setFilter(filter: string) {
 }
 function sortTodos() {
 	sortState.value = (sortState.value + 1) % 3;
-	console.log("sortTodos", sortState.value);
+	if (sortState.value === SortState.UNSORTED) {
+		console.log("sortTodos", "SortState.UNSORTED");
+	}
+	if (sortState.value === SortState.ASCENDING) {
+		console.log("sortTodos", "SortState.ASCENDING");
+	}
+	if (sortState.value === SortState.DESCENDING) {
+		console.log("sortTodos", "SortState.DESCENDING");
+	}
 	todoStore.setSort(sortState.value);
 }
 console.log("hello from app.vue", todoStore.todos);
